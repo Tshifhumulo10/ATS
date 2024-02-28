a378916b0be652478c829da50ab339a382dbde0c
Tshifhumulo Mapasa <115041717+Tshifhumulo10@users.noreply.github.com>
2024-02-28 18:33:28 +0200

    Add files via upload

---
+++ ATS.py
@@ -0,0 +1,63 @@
+
+import docx2txt as doc
+from pdfminer.high_level import extract_text
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import streamlit as st
+import matplotlib.pyplot as plt
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import string
+
+st.title("Application Tracking System")
+
+uploaded_CV  = st.text_area("Paste your CV here:", height=100)
+uploaded_job = st.text_area("Paste  job description:",height=100)
+button_clicked = st.button("Score", key="predict_button", kwargs={"style": "background-color: red; color: red"})
+
+if button_clicked:
+    def ATS(uploaded_CV, uploaded_job):
+        
+        if uploaded_CV is None or uploaded_job is None:
+            return "Please insert text in both fields."
+        else:
+       
+            text_cv= [uploaded_CV]
+            text_job= [uploaded_job]
+
+            # Fit and transform the vectorizer on individual documents
+            tfidf = TfidfVectorizer(stop_words="english", ngram_range=(1, 1))
+            tfidf_cv = tfidf.fit_transform(text_job)
+            tfidf_job = tfidf.transform(text_cv)
+
+            # Calculate cosine similarity
+            ats = cosine_similarity(tfidf_cv, tfidf_job)[0][0]
+            ats *= 100
+            return ats
+        
+    result = ATS(uploaded_CV, uploaded_job)
+    st.subheader("SCORE")
+    st.write(result)
+    
+    def find_missing_words(uploaded_CV, uploaded_job):
+    
+        work_tokens = set(word_tokenize(uploaded_job.lower()))
+        cv_tokens = set(word_tokenize(uploaded_CV.lower()))
+
+        stop_words = set(stopwords.words('english'))
+        work_tokens = work_tokens.difference(stop_words)
+        cv_tokens = cv_tokens.difference(stop_words)
+        
+        punctuation = set(string.punctuation)
+        work_tokens = work_tokens.difference(punctuation)
+        cv_tokens = cv_tokens.difference(punctuation)
+
+        missing_words = work_tokens.difference(cv_tokens)
+        
+        return missing_words
+    
+    result = find_missing_words(uploaded_CV, uploaded_job)
+    
+    st.subheader("Missing Words")
+    st.write(result)
